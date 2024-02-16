f28ea7c4a05eeaf049ca980ea67173802d43ed2a
Diwas Rai <diwasrai38@yahoo.com>
2024-02-16 19:09:57 +0000

    add styling for CluckBox component

--- client/src/components/CluckBox.jsx
+++ client/src/components/CluckBox.jsx
@@ -1,12 +1,27 @@
+import "./CluckBox.css";
+import profilePicUrl from "/public/images/default-pic.jpg";
+
 const CluckBox = ({ cluck }) => {
   return (
     <div className="cluckBox">
-      <h4>{cluck._id}</h4>
-      <p>{cluck.text}</p>
-      {cluck.updatedAt != cluck.createdAt && (
-        <p>Last edited: {cluck.updatedAt}</p>
-      )}
-      <p>{cluck.createdAt}</p>
+      <div className="cluck-header">
+        <img src={profilePicUrl} alt="Profile" className="profile-pic" />
+        <div className="name-username">
+          <h4 className="name">Name</h4>
+          <h4 className="username">@username</h4>
+        </div>
+      </div>
+      <div className="cluck-content">
+        <p>{cluck.text}</p>
+      </div>
+      <div className="cluck-info">
+        <div className="cluck-timestamp">
+          <p>Posted at: {new Date(cluck.createdAt).toLocaleString()}</p>
+          {cluck.updatedAt != cluck.createdAt && (
+            <p>Last edited: {new Date(cluck.updatedAt).toLocaleString()}</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
