d729d272cb755629943d25872a491f3ef55c1eae
Diwas Rai <diwas.rai@kcl.ac.uk>
2024-02-23 18:07:56 +0000

    add ability to create user sessions

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,86 +1,94 @@
 //logic file/code for the routes/ api end points
-const User = require('../models/user')
-const {hashPassword, comparePassword} = require('../helpers/auth')
+const User = require("../models/user");
+const {
+  hashPassword,
+  comparePassword,
+  createSecretToken,
+} = require("../helpers/auth");
 
-const test = (req,res) =>
-{
-    res.json('test is working')
-}
+const test = (req, res) => {
+  res.json("test is working");
+};
 
 // Register endpoint
-const registerUser = async (req, res) =>
-{
-    try {
-        const {userName, email, password} = req.body;
-        //check if userName was entered
-        if(!userName){
-            return res.json({
-                error: 'User Name is required'
-            })
-        };
-        //check if password meets criteria
-        if(!password || password.length<6){
-            return res.json({
-                error: 'Password is required and should be at least 6 characters long'
-            })
-        };
-        //check email if it is already register in the database
-        const exist = await User.findOne({email})
-        if(exist){
-            return res.json({
-                error: 'Email is already taken'
-
-            })
-            
-        }
-        
-        const hashedPassword = await hashPassword(password)
-        //create user in database
-        // still needs to hash the password
-        const user = await User.create({
-            userName, 
-            email, 
-            password : hashedPassword,
-        })
-
-        return res.json(user)
-    } catch (error) {
-        console.log(error)
-        
+const registerUser = async (req, res) => {
+  try {
+    const { userName, email, password } = req.body;
+    //check if userName was entered
+    if (!userName) {
+      return res.json({
+        error: "User Name is required",
+      });
     }
-}
+    //check if password meets criteria
+    if (!password || password.length < 6) {
+      return res.json({
+        error: "Password is required and should be at least 6 characters long",
+      });
+    }
+    //check email if it is already register in the database
+    const exist = await User.findOne({ email });
+    if (exist) {
+      return res.json({
+        error: "Email is already taken",
+      });
+    }
+
+    const hashedPassword = await hashPassword(password);
+    //create user in database
+    // still needs to hash the password
+    const user = await User.create({
+      userName,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.json(user);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 //Login endpoint
-const loginUser = async (req,res) => {
-    try {
-        const {email, password} = req.body;
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-        //Check if user exists
-        const user = await User.findOne({email});
-        if(!user){
-            return res.json({
-                error : 'No user found'
-            })
-        }
-        //check if password match
-        const match = await comparePassword(password,user.password)
-        if(match){
-            res.json('Password match')
-        }
-        if(!match){
-            res.json({
-                error: 'Passwords do not match'
-            })
-        }
-        } catch (error) {
-            console.log(error)
-        
+    //check if email and password were entered
+    if (!email || !password) {
+      return res.json({ message: "All fields are required" });
     }
 
-}
-module.exports = 
-{
-    test,
-    registerUser,
-    loginUser
-}
+    //Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({
+        error: "No user found",
+      });
+    }
+    //check if password match
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      res.json({
+        error: "Passwords do not match",
+      });
+    }
+
+    //create token
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({ message: "User logged in successfully", success: true });
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = {
+  test,
+  registerUser,
+  loginUser,
+};
--- server/helpers/auth.js
+++ server/helpers/auth.js
@@ -1,25 +1,52 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-const hashPassword = (password) =>{
-    return new Promise((resolve,reject) => {
-        bcrypt.genSalt(12,(err, salt)=>{
-            if(err){
-                reject(err)
-            }
-            bcrypt.hash(password,salt,(err,hash)=>{
-                if(err){
-                    reject(err)
-                }
-                resolve(hash)
-            })
-        })
-    })
-}
+const hashPassword = (password) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(12, (err, salt) => {
+      if (err) {
+        reject(err);
+      }
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(hash);
+      });
+    });
+  });
+};
 
-const comparePassword = (password,hashed) => {
-    return bcrypt.compare(password, hashed)
-}
-module.exports ={
-    hashPassword,
-    comparePassword
-}
+const comparePassword = (password, hashed) => {
+  return bcrypt.compare(password, hashed);
+};
+
+const createSecretToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: 3 * 24 * 60 * 60,
+  });
+};
+
+const userVerification = (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.json({ status: false });
+  }
+  jwt.verify(token, process.env.JWT_SECRET, async (err, data) => {
+    if (err) {
+      return res.json({ status: false });
+    } else {
+      const user = await User.findById(data.id);
+      if (user) return res.json({ status: true, user: user.username });
+      else return res.json({ status: false });
+    }
+  });
+};
+
+module.exports = {
+  hashPassword,
+  comparePassword,
+  createSecretToken,
+  userVerification
+};
