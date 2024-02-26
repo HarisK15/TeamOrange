49ef512ace536a589ff5c8e0d6cc6cb97f073ad3
Diwas Rai <diwasrai38@yahoo.com>
2024-02-26 01:24:59 +0000

    update loginUser.test.js

--- server/tests/loginUser.test.js
+++ server/tests/loginUser.test.js
@@ -1,76 +1,68 @@
-const supertest = require('supertest');
-const app = require('../index.js'); 
+const supertest = require("supertest");
+const app = require("../index.js");
 const request = supertest(app);
-const mongoose = require('mongoose');
-const { MongoMemoryServer } = require('mongodb-memory-server');
-const User = require('../models/user.js'); 
-const {hashPassword, comparePassword} = require('../helpers/auth.js')
-
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const User = require("../models/user.js");
+const { hashPassword, comparePassword } = require("../helpers/auth.js");
 
 let mongoServer;
 
 beforeAll(async () => {
-    if (mongoose.connection.readyState === 1 || mongoose.connection.readyState === 2) {
-      await mongoose.disconnect();
-    }
-    mongoServer = await MongoMemoryServer.create();
-    const mongoUri = mongoServer.getUri();
-    await mongoose.connect(mongoUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  });
-  
-afterAll(async () => {
+  if (
+    mongoose.connection.readyState === 1 ||
+    mongoose.connection.readyState === 2
+  ) {
     await mongoose.disconnect();
-    await mongoServer.stop();
-  });
-
-
-  beforeEach(async () => {
-    // Clear the users collection before each test
-    await User.deleteMany({});
-    
-    // Creating a user
-    const hashedPassword = await hashPassword('password123');
-    await User.create({
-        email: 'test@example.com',
-        password: hashedPassword, 
-        userName: 'testUser'
-    });
+  }
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+  await mongoose.connect(mongoUri);
 });
 
-describe('User Login', () => {
-    it('should log in an existing user successfully', async () => {
-        const response = await request
-            .post('/login') 
-            .send({
-                email: 'test@example.com',
-                password: 'password123',
-            });
-        expect(response.statusCode).toBe(200);
-        expect(response.body).toEqual('Password match'); 
-    });
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
 
-    it('should fail if the email does not exist', async () => {
-        const response = await request
-            .post('/login')
-            .send({
-                email: 'nonexistent@example.com',
-                password: 'password123',
-            });
-        expect(response.statusCode).toBe(200); 
-        expect(response.body).toHaveProperty('error', 'No user found');
-    });
+beforeEach(async () => {
+  // Clear the users collection before each test
+  await User.deleteMany({});
 
-    it('should fail if the password is incorrect', async () => {
-        const response = await request
-            .post('/login')
-            .send({
-                email: 'test@example.com',
-                password: 'wrongpassword',
-            });
-        expect(response.statusCode).toBe(200); 
-        expect(response.body).toHaveProperty('error', 'Passwords do not match');
+  // Creating a user
+  const hashedPassword = await hashPassword("password123");
+  await User.create({
+    email: "test@example.com",
+    password: hashedPassword,
+    userName: "testUser",
+  });
+});
+
+describe("User Login", () => {
+  it("should log in an existing user successfully", async () => {
+    const response = await request.post("/login").send({
+      email: "test@example.com",
+      password: "password123",
     });
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toHaveProperty("success", true);
+  });
+
+  it("should fail if the email does not exist", async () => {
+    const response = await request.post("/login").send({
+      email: "nonexistent@example.com",
+      password: "password123",
+    });
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty("error", "No user found");
+  });
+
+  it("should fail if the password is incorrect", async () => {
+    const response = await request.post("/login").send({
+      email: "test@example.com",
+      password: "wrongpassword",
+    });
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty("error", "Passwords do not match");
+  });
 });
