ed79f119d1b850bfbd6d776e5e6c530aa155b86a
Jason Cai <jiaxin.cai@kcl.ac.uk>
2024-02-27 12:10:42 +0000

    Add front end for home page

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -59,7 +59,7 @@ const loginUser = async (req,res) => {
         const user = await User.findOne({email});
         if(!user){
             return res.json({
-                error : 'No user found'
+                error : 'No user found, please register first'
             })
         }
         //check if password match
