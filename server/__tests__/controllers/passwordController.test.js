7429448690e277c988907af4acea61f29bf4f801
Diwas Rai <diwasrai38@yahoo.com>
2024-03-04 01:38:19 +0000

    refactor passwordController.test.js
    Co-authored-by: Deividas Juska <djuska16@gmail.com>

--- server/__tests__/controllers/passwordController.test.js
+++ server/__tests__/controllers/passwordController.test.js
@@ -1,163 +1,110 @@
-const app = require("../../app.js");
-const mongoose = require("mongoose");
-const { MongoMemoryServer } = require("mongodb-memory-server");
-const User = require("../../models/user.js");
-const { hashPassword } = require("../../helpers/auth.js");
-const { mockRequest, mockResponse } = require("jest-express");
-const { changePassword } = require("../../controllers/passwordController.js");
+const { changePassword } = require("../../controllers/passwordController");
+const User = require("../../models/user");
+const { hashPassword } = require("../../helpers/auth");
 
-let mongoServer;
+jest.mock("../../models/user");
 
-beforeAll(async () => {
-  if (
-    mongoose.connection.readyState === 1 ||
-    mongoose.connection.readyState === 2
-  ) {
-    await mongoose.disconnect();
-  }
-  mongoServer = await MongoMemoryServer.create();
-  const mongoUri = mongoServer.getUri();
-  await mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+const createMockReqRes = (currentPassword, newPassword) => ({
+  req: {
+    body: { currentPassword, newPassword },
+  },
+  res: {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  },
 });
 
-afterAll(async () => {
-  await mongoose.disconnect();
-  await mongoServer.stop();
+const createMockUser = async (password, saveReturnValue) => ({
+  password: await hashPassword(password),
+  save: jest.fn().mockResolvedValue(saveReturnValue),
 });
 
-beforeEach(async () => {
-  await User.deleteMany({});
-
-  const hashedPassword = await hashPassword("password123");
-  await User.create({
-    email: "test@example.com",
-    password: hashedPassword,
-    userName: "testUser",
-  });
+// These hooks suppress console.error output during tests to keep the test output clean
+beforeEach(() => {
+  jest.spyOn(console, "error").mockImplementation(() => {});
+});
+afterEach(() => {
+  console.error.mockRestore();
 });
 
-const userData = {
-  currentPassword: "password123",
-  newPassword: "Password123",
-};
+describe("changePassword", () => {
+  it("should return 404 if user not found", async () => {
+    User.findById.mockResolvedValue(null);
 
-describe("Change Password API", () => {
-  it("should change user password when provided with valid credentials", async () => {
-    const req = mockRequest();
-    const res = mockResponse();
-    req.session.userEmail = "test@example.com";
-    req.body = {
-      ...userData,
-    };
+    const { req, res } = createMockReqRes("oldPassword", "newPassword");
+
     await changePassword(req, res);
 
-    expect(res.statusCode).toBe(200);
-    expect(res.json).toEqual({
-      success: true,
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({ error: "User not found" });
+  });
+
+  it("should return 400 if current password is incorrect", async () => {
+    const mockUser = await createMockUser("oldPassword", false);
+    User.findById.mockResolvedValue(mockUser);
+
+    const { req, res } = createMockReqRes("wrongPassword", "newPassword");
+
+    await changePassword(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      error: "Current password is incorrect",
+    });
+  });
+
+  it("should return 400 if new password is too short", async () => {
+    const mockUser = await createMockUser("oldPassword", false);
+    User.findById.mockResolvedValue(mockUser);
+
+    const { req, res } = createMockReqRes("oldPassword", "new");
+
+    await changePassword(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      error:
+        "A new password is required and should be at least 6 characters long",
+    });
+  });
+
+  it("should return 400 if new password is the same as the old password", async () => {
+    const mockUser = await createMockUser("oldPassword", false);
+    User.findById.mockResolvedValue(mockUser);
+
+    const { req, res } = createMockReqRes("oldPassword", "oldPassword");
+
+    await changePassword(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      error:
+        "A new password is required and should be at least 6 characters long",
+    });
+  });
+
+  it("should return 200 if password is updated successfully", async () => {
+    const mockUser = await createMockUser("oldPassword", true);
+    User.findById.mockResolvedValue(mockUser);
+
+    const { req, res } = createMockReqRes("oldPassword", "newPassword");
+
+    await changePassword(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
       message: "Password updated successfully",
     });
   });
 
-  it("should return error 400 when provided with an empty new password", async () => {
-    const req = mockRequest();
-    const res = mockResponse();
-    req.session.userEmail = "test@example.com";
-    req.body = {
-      ...userData,
-      newPassword: "",
-    };
+  it("should return 500 if an error occurs", async () => {
+    User.findById.mockRejectedValue("error");
+
+    const { req, res } = createMockReqRes("oldPassword", "newPassword");
+
     await changePassword(req, res);
 
-    expect(res.statusCode).toBe(400);
-    expect(res.json).toEqual({
-      success: false,
-      message:
-        "A new password is required and should be at least 6 characters long",
-    });
-  });
-
-  it("should return error 400 when provided with a password shorter than 6", async () => {
-    const req = mockRequest();
-    const res = mockResponse();
-    req.session.userEmail = "test@example.com";
-    req.body = {
-      ...userData,
-      newPassword: "short",
-    };
-    await changePassword(req, res);
-
-    expect(res.statusCode).toBe(400);
-    expect(res.json).toEqual({
-      success: false,
-      message:
-        "A new password is required and should be at least 6 characters long",
-    });
-  });
-
-  it("should return error 404 when an email provided is not valid", async () => {
-    const req = mockRequest();
-    const res = mockResponse();
-    req.body = {
-      ...userData,
-    };
-    await changePassword(req, res);
-
-    expect(res.statusCode).toBe(404);
-    expect(res.json).toEqual({ success: false, message: "User not found" });
-  });
-
-  it("should return error 400 when provided with a current password and new password that are equal to eachother", async () => {
-    const req = mockRequest();
-    const res = mockResponse();
-    req.session.userEmail = "test@example.com";
-    req.body = {
-      ...userData,
-      newPassword: "password123",
-    };
-    await changePassword(req, res);
-
-    expect(res.statusCode).toBe(400);
-    expect(res.json).toEqual({
-      success: false,
-      message:
-        "A new password is required and should be at least 6 characters long",
-    });
-  });
-
-  it("should return error 400 when provided with a current password that does not match their password in the database", async () => {
-    const req = mockRequest();
-    const res = mockResponse();
-    req.session.userEmail = "test@example.com";
-    req.body = {
-      ...userData,
-      currentPassword: "Password123",
-    };
-    await changePassword(req, res);
-
-    expect(res.statusCode).toBe(400);
-    expect(res.json).toEqual({
-      success: false,
-      message: "Current password is incorrect",
-    });
-  });
-
-  it("should return error 400 when provided with a current password that is empty", async () => {
-    const req = mockRequest();
-    const res = mockResponse();
-    req.session.userEmail = "test@example.com";
-    req.body = {
-      ...userData,
-      currentPassword: "",
-    };
-    await changePassword(req, res);
-
-    expect(res.statusCode).toBe(400);
-    expect(res.json).toEqual({
-      success: false,
-      message: "Current password is incorrect",
-    });
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({ error: "Internal server error" });
   });
 });
